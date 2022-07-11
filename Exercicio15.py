1735e766d8ea89868d2dabd1ea8a069d34eab35f
Rhumenigue <rhumenigue@gmail.com>
2022-07-11 17:18:32 -0300

    Update Exercicio15.py

--- Exercicio15.py
+++ Exercicio15.py
@@ -1,9 +1,9 @@
 x=int(input())
-print (f'{int(x)}')
-while x|-1:
-    if x%2 == and x !=2:
-        print(f'{int(x/2)'}
+print(f'{int(x)}')
+while x!=1:
+    if x%2 == 0 and x != 0:
+        print(f'{int(x/2)}')
         x=x/2
     else:
-        print(f'{int(x*3+2)}')
+        print(f'{int(x*3+1)}')
         x=x*3+1
